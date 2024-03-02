b007de79c08c91871eb954d30943c614038ea0a7
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-03-02 16:20:51 +0100

    kk-implemented-services

---
+++ HG.Infrastructure/Services/PostService/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using HG.Infrastructure.RequestModels;
+using HG.Infrastructure.ResponseModel;
+
+namespace HG.Infrastructure.Services.PostService
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<PostResponseModel>> GetAllPosts();
+        Task<PostResponseModel> GetPostById(int postId);
+        Task<PostResponseModel> AddPost(PostRequestModel post);
+        Task<PostResponseModel> UpdatePost(int postId, PostRequestModel post);
+        Task<SuccessResponseModel> DeletePost(int postId);
+    }
+}
