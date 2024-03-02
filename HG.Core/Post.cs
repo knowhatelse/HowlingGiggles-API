370a4a542664bdb90bd49a4178fa978184531c00
Mario Polic <75645353+mariopolic@users.noreply.github.com>
2024-03-02 14:07:29 +0100

    MP - implemented models

---
+++ HG.Core/Post.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HG.Core;
+
+public class Post
+{
+    [Key]
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PostContent { get; set; }
+    public bool isDeleted { get; set; }
+}
---
+++ HG.Infrastructure/ResponseModel/PostResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Infrastructure.ResponseModel
+{
+    public class PostResponseModel
+    {
+        public string Username { get; set; }
+        public string PostContent { get; set; }
+    }
+}
